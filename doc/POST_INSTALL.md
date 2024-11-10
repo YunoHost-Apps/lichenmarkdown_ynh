9d670aa7d2a919cfae960b3bd9ae8f8fdd268806
notplants <mfowler.email@protonmail.com>
2024-11-10 14:56:29 -0800

    working on documentation

--- doc/POST_INSTALL.md
+++ doc/POST_INSTALL.md
@@ -1,1 +1,1 @@
-Login with `admin/admin`, be sure to change these credentials after your first login.
+Go to yourdomain.tld/cms/edit.php and login with username "admin" and whatever password you set during the installation process to see the admin panel. 
--- nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#rsync -avzh -e "ssh -i /home/notplants/.ssh/do_rsa2" /home/notplants/computer/projects/thimble.commoninternet.net/ root@147.182.177.135:/srv/thimble.commoninternet.net/
-rsync -avzh -e "ssh -i /home/notplants/.ssh/do_rsa3" /home/notplants/computer/projects/lichen-markdown/lichenmarkdown_ynh/conf/test3.conf np@b1.net:/etc/nginx/conf.d/lm.commoninternet.net.d/lichenmarkdown.conf
-
-#ssh -tt -i /home/notplants/.ssh/do_rsa3 np@b1.net << EOF
-#sudo systemctl restart nginx
-#EOF
-
-#rsync -avzh -e "ssh -i ~/.ssh/do_rsa2" /home/notplants/computer/projects/lichen-markdown/lichenmarkdown_ynh/ root@147.182.177.135:/srv/lichenmarkdown_ynh/
